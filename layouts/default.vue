9f89e422ef0e98e00a874a07b74f709a8e34b489
David Sabalete Rodríguez <dsabalete@gmail.com>
2021-12-15 12:02:06 +0100

    feat: set title for desktop site

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-row">
+    <div class="flex flex-col sm:flex-row">
         <Nav />
         <nuxt />
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
-    <main>
-        About
+    <main class="px-4">
+        <h1 class="hidden sm:block text-5xl uppercase font-bold text-blue-900">
+            <span class="text-black">David</span> Sabalete Rodríguez
+        </h1>
+        <h2 class="text-center">
+            <a
+                href="mailto:dsabalete@gmail.com"
+                class="font-extralight text-blue-900"
+                ><span class="text-black">dsabalete</span><b>@</b>gmail.com</a
+            >
+        </h2>
+        <About />
         <hr />
         Experience
         <hr />
