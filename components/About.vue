0f862c318fead2d64657a2c313fe979327641fe8
David Sabalete Rodríguez <dsabalete@gmail.com>
2021-12-15 12:01:12 +0100

    feat: create About section

---
+++ components/About.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="my-8" id="about">
+        <p>
+            I am experienced in Vue.js and Nuxt.js in which I produce high
+            quality code for my clients that are both well performing and well
+            tested. I focus on solving problems, not only technical problems but
+            creating an awesome experience for the users of my software both
+            visual and accessible. Mixing powerful tools and knowledge about the
+            users brings visual UI's to life and leads to webpages being
+            accessible and useful.
+        </p>
+
+        <SocialIcons />
+    </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
