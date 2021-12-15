991f0b5c82350609e3840c3bc72c38b9bccadb9e
David Sabalete Rodríguez <dsabalete@gmail.com>
2021-12-15 12:00:51 +0100

    feat: create Nav side bar

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>Nav</div>
+    <nav
+        class="navbar right-0 left-0 z-20 sm:w-60 sm:h-screen sm:static sm:bg-blue-900 items-center"
+        id="sideNav"
+    >
+        <div class="sm:w-60 self-center">
+            <div
+                class="fixed top-0 w-full text-center text-xl bg-blue-900 text-white p-4 sm:hidden"
+            >
+                David Sabalete Rodríguez
+            </div>
+            <div class="mt-28">
+                <img
+                    alt="Image of David"
+                    class="rounded-full border-8 border-blue-100 mx-auto my-4 w-2/3"
+                    src="../assets/images/profile.jpg"
+                />
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
