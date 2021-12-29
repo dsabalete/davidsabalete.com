9ffcb99685113e9da1c0fcee5725e9b336146d74
David Sabalete Rodríguez <dsabalete@gmail.com>
2021-12-29 15:08:28 +0100

    refactor: list of projects

---
+++ components/Project.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="project">
+        <div class="project-image">
+            <img :src="project.image" :alt="project.alt" />
+        </div>
+        <div class="project-info">
+            <h3>{{ project.title }}</h3>
+            <p>{{ project.description }}</p>
+            <a :href="project.repo" class="view-project" target="_blank"
+                >View Project</a
+            >
+            <a :href="project.live" class="view-demo" target="_blank">Demo</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Project",
+    props: {
+        project: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/projects.js
@@ -0,0 +1,139 @@
+export const projects = [
+    {
+        image: "/space-tourism.png",
+        alt: "Space tourism homepage picture",
+        title: "Space tourism",
+        description:
+            "A website for a space tourism company. It is a static website with responsive design, plain CSS and a bit of vanilla javascript.",
+        repo: "https://github.com/dsabalete/space-tourism",
+        live: "https://sabalete-space-tourism.netlify.app"
+    },
+    {
+        image: "/kiloday.png",
+        alt: "Kiloday homepage picture",
+        title: "Kiloday calculator",
+        description:
+            "Alternative birthday calculator app made with Vue, Vite and Pinia",
+        repo: "",
+        live: "https://kiloday.netlify.app"
+    },
+    {
+        image: "https://dsabalete.github.io/images/vue-twitter-clone.png",
+        alt: "Vue twitter clone homepage picture",
+        title: "Vue twitter clone",
+        description:
+            "App that emulates basic functionality of a Twitter app. Made with Vue and deployed on Heroku.",
+        repo: "https://github.com/dsabalete/vue-twitter-clone",
+        live: "https://elegant-rosalind-90c586.netlify.app/"
+    },
+    {
+        image: "https://dsabalete.github.io/images/expensify.png",
+        alt: "Expensify homepage picture",
+        title: "Expensify App",
+        description:
+            "App that helps keep you track of your expenses. Made with React and Firebase.",
+        repo: "https://github.com/dsabalete/udemy-expensify-app",
+        live: "https://raspbian-expensify.herokuapp.com/"
+    },
+    {
+        image: "https://dsabalete.github.io/images/indecision.png",
+        alt: "Indecision homepage picture",
+        title: "Indecision App",
+        description:
+            "Put your live in the hands of a computer. Made with React.",
+        repo: "https://github.com/dsabalete/udemy-indecision-app",
+        live: "https://raspbian-indecision.herokuapp.com"
+    },
+    {
+        image: "https://dsabalete.github.io/images/portfolio.png",
+        alt: "Portfolio on Github",
+        title: "Gridsome-Portfolio",
+        description: "My Portfolio made with Gridsome and ❤️",
+        repo: "https://github.com/dsabalete/gridsome-portfolio",
+        live: ""
+    },
+    {
+        image: "https://dsabalete.github.io/images/creualta.png",
+        alt: "Creualta homepage picture",
+        title: "Creu Alta website",
+        description: `Creu Alta Sabadell Basket website. It is running on
+                        Drupal as CMS an it has a VueJS app to register players
+                        during summer and christmas campus.`,
+        repo: "",
+        live: "https://www.creualtabasquet.cat"
+    },
+    {
+        image: "https://dsabalete.github.io/images/my-1-rm.png",
+        alt: "My 1 RM homepage picture",
+        title: "My 1 RM website",
+        description: `App that calculates the 1RM of any training session.
+                        Made with Vuejs and hosted in Netlify.`,
+        repo: "https://github.com/dsabalete/my-1-rm",
+        live: "https://priceless-fermat-717abc.netlify.app/"
+    },
+    {
+        image: "https://dsabalete.github.io/images/calculator.png",
+        alt: "ReactJS Calculator picture",
+        title: "ReactJS Calculator",
+        description: `ReactJS Calculator made with React.js. It has a
+                        simple calculator with a few functions.`,
+        repo: "https://codepen.io/dsabalete/full/yZrxzP",
+        live: ""
+    },
+    {
+        image: "https://dsabalete.github.io/images/drum_machine.png",
+        alt: "React and Redux Drum machine picture",
+        title: "Drum machine",
+        description: `React and Redux Drum machine made with React and Redux.`,
+        repo: "https://codepen.io/dsabalete/full/bzQEqQ",
+        live: ""
+    },
+    {
+        image: "https://dsabalete.github.io/images/markdown.png",
+        alt: "Markdown previewer picture",
+        title: "Markdown previewer",
+        description: `Markdown previewer made with React.js and Redux`,
+        repo: "https://codepen.io/dsabalete/full/daqqBQ",
+        live: ""
+    },
+    {
+        image: "https://dsabalete.github.io/images/pomodoro.png",
+        alt: "Pomodoro clock picture",
+        title: "Pomodoro clock",
+        description: `Pomodoro clock made with React.js and Redux`,
+        repo: "https://codepen.io/dsabalete/full/MLNQvQ",
+        live: ""
+    },
+    {
+        image: "https://dsabalete.github.io/images/quote_machine.png",
+        alt: "Quote Machine picture",
+        title: "Quote Machine",
+        description: "Random Quote Machine using external API",
+        repo: "https://codepen.io/dsabalete/full/QdYLOZ",
+        live: ""
+    },
+    {
+        image: "https://dsabalete.github.io/images/tribute_page.png",
+        alt: "Bruce Lee tribute page",
+        title: "Bruce Lee tribute page",
+        description: "Simple Tribute page made with Bootstrap",
+        repo: "https://codepen.io/dsabalete/full/aBPxzB",
+        live: ""
+    },
+    {
+        image: "https://dsabalete.github.io/images/weather.png",
+        alt: "Weather app picture",
+        title: "Weather app",
+        description: "Forecast Weather App using JQuery and OpenWeatherMap API",
+        repo: "https://codepen.io/dsabalete/full/RKvqjJ",
+        live: ""
+    },
+    {
+        image: "https://dsabalete.github.io/images/wikipedia_viewer.png",
+        alt: "Wikipedia viewer picture",
+        title: "Wikipedia viewer",
+        description: "Wikipedia Viewer App made with Angular and Wikipedia API",
+        repo: "https://codepen.io/dsabalete/full/OpRKEZ",
+        live: ""
+    }
+]
