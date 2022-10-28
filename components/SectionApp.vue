3016781b439248dd2ba699c3817c69041e43b75d
David Sabalete Rodríguez <dsabalete@gmail.com>
2022-10-28 19:23:29 +0200

    feat: do not render h2 if there is no slot title

--- components/SectionApp.vue
+++ components/SectionApp.vue
@@ -1,10 +1,24 @@
 <template>
-    <div class="border-b border-gray-300 py-10 px-4 sm:px-8 sm:min-h-screen">
+    <section
+        class="border-b border-gray-300 py-10 px-4 sm:px-8 sm:min-h-screen"
+    >
         <h2
+            v-if="hasHeader"
             class="font-title text-4xl font-bold text-blue-900 uppercase pt-8 pb-16"
         >
             <slot name="title" />
         </h2>
+
         <slot />
-    </div>
+    </section>
 </template>
+
+<script>
+export default {
+    computed: {
+        hasHeader() {
+            return !!this.$slots.title
+        }
+    }
+}
+</script>
