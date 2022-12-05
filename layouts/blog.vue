77a343f65f822f0948c6a924bbd7f62b66729134
David Sabalete Rodríguez <dsabalete@gmail.com>
2022-12-05 23:21:27 +0100

    feat: add home link in posts

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,10 +1,15 @@
 <template>
-    <div class="bg-blue-100 pt-4">
+    <div class="bg-blue-100">
         <nuxt-link
             to="/"
-            class="m-4 text-2xl pt-8 uppercase font-bold text-blue-900"
+            class="mt-2 text-2xl uppercase font-bold text-blue-900 sticky top-2 left-0"
         >
-            👈 🏠 HOME
+            <img
+                v-tooltip.right="`Go back home`"
+                src="/icons/home.svg"
+                alt="Home"
+                class="h-8 ml-2"
+            />
         </nuxt-link>
         <div
             class="sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl mx-auto p-4"
