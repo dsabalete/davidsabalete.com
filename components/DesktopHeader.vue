eb8902b17d870f972ca389c24eb1d7cf5a9a2b89
David <dsabalete@gmail.com>
2026-02-25 13:53:52 +0100

    chore: update experise header

--- components/DesktopHeader.vue
+++ components/DesktopHeader.vue
@@ -3,16 +3,16 @@ const { t } = useI18n()
 const { trackEvent } = useAnalytics()
 
 const handleContactClick = () => {
-  trackEvent('contact_button_click', {
-    button_location: 'header',
-    button_text: t('header_contact')
+  trackEvent("contact_button_click", {
+    button_location: "header",
+    button_text: t("header_contact")
   })
 }
 
 const handleDownloadCvClick = () => {
-  trackEvent('download_cv_click', {
-    button_location: 'header',
-    button_text: t('header_download_cv')
+  trackEvent("download_cv_click", {
+    button_location: "header",
+    button_text: t("header_download_cv")
   })
 }
 
@@ -43,7 +43,7 @@ const personSchema = {
     "Git",
     "GitHub",
     "React",
-    "Angular",
+    // "Angular",
     "PHP",
     "Drupal",
     "WordPress",
@@ -102,10 +102,10 @@ useHead({
       <div class="text-base my-2 text-gray-700 dark:text-gray-300" itemprop="knowsAbout">
         <span class="font-semibold">{{ t("header_expertise") }}</span
         >&nbsp; <span class="inline-block"> Vue.js </span>, <span class="inline-block"> Nuxt.js </span>,
-        <span class="inline-block"> JavaScript </span>, <span class="inline-block"> TypeScript </span>,
-        <span class="inline-block"> Node.js </span>, <span class="inline-block"> AWS </span>,
-        <span class="inline-block"> Terraform </span>, <span class="inline-block"> Git </span>,
-        <span class="inline-block"> GitHub </span>
+        <span class="inline-block"> React </span>, <span class="inline-block"> JavaScript </span>,
+        <span class="inline-block"> TypeScript </span>, <span class="inline-block"> Node.js </span>,
+        <span class="inline-block"> Docker </span>, <span class="inline-block"> AWS </span>,
+        <span class="inline-block"> Terraform </span>
       </div>
       <p
         class="text-black dark:text-gray-400 text-sm"
@@ -128,8 +128,8 @@ useHead({
           aria-label="Email David Sabalete"
         >
           <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
-            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/>
+            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
           </svg>
           {{ t("header_hire_me") }}
         </a>
